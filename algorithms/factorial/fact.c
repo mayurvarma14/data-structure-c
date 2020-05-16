89224361be8942eb2d197fb7f33302eb26292451
Mayur Varma <mayurvarma14@gmail.com>
2020-05-16 16:46:27 +0530

    refactor:Remove unnecessary elseif in facttail

--- algorithms/factorial/fact.c
+++ algorithms/factorial/fact.c
@@ -21,11 +21,7 @@ long facttail(long n, long a)
     {
         return 0;
     }
-    else if (n == 0)
-    {
-        return 1;
-    }
-    else if (n == 1)
+    else if (n == 0 || n == 1)
     {
         return a;
     }
